8e2e3da81e1159966dd750260786ac29719dd031
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-17 00:22:11 -0700

    adjust score

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,14 +9,16 @@ function Footer(props) {
             {/* Render the check answers button if the game is still ongoing */}
             {
                 !props.showAnswers &&
-                <button className="btn btn-black btn-submit" onClick={props.checkAnswers}>Check answers</button>
+                <div className="footer-container1"> 
+                    <button className="btn btn-black btn-submit" onClick={props.checkAnswers}>Check answers</button>
+                </div>
             }
 
             {/* Renders the score and play again button if player is checking their answers after the game */}
             {
                 props.showAnswers &&
-                <div className="footer-container">
-                    <p>Score: {props.score} / {props.questions.length()}</p>
+                <div className="footer-container2">
+                    <p className="score">You scored {props.score} / {props.questions.length} correct answers</p>
                     <button className="btn btn-black btn-submit" onClick={props.playAgain}>Play Again</button>
                 </div>
             }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -4,19 +4,16 @@ import {nanoid} from 'nanoid'
 
 function Question(props) {
 
-    // Create state for the player's answer
-    const [playerAnswer, setPlayerAnswer] = React.useState(null);
-
     // Create state for the shuffled answers array
     const [shuffledAllAnswers, setShuffledAllAnswers] = React.useState([]);
 
     // Add the incorrect and correct answers for the question to a new answers array
     const allAnswers = [...props.incorrectAnswers, props.correctAnswer];
 
-    // Shuffle the answers array when the component mounts or when props change
+    // Shuffle the answers array when the component first mounts 
     React.useEffect(() => {
         setShuffledAllAnswers(shuffle(allAnswers));
-    }, [allAnswers]);
+    }, []);
 
     // Function to shuffle the answers array
     function shuffle(array) {
@@ -33,26 +30,17 @@ function Question(props) {
         return (
             <div key={id} className="button-group">
                 <input type="radio" id={id} name={`answer-${props.id}`} value={answer} />
-                <label htmlFor={id}>{decode(answer)}</label>
+                <label onSelect={() => props.handleAnswerSelect(props.id, answer)} htmlFor={id}>{decode(answer)}</label>
             </div>
         );
     });
-
-    // If the player is checking their answers, save the player's selected answer
-    React.useEffect(() => {
-        if (props.showAnswers) {
-            const selectedInput = document.querySelector(`input[name="answer-${props.id}"]:checked`);
-            const answer = selectedInput ? selectedInput.value : null;
-            console.log(answer);
-            setPlayerAnswer(answer);
-    }, [props.showAnswers]);
 
     // Create an array of checked answer elements (to reflect the player's answers as correct or incorrect)
     const checkedAllAnswerElements = shuffledAllAnswers.map(answer => {
         const id = nanoid();
 
         // If the player's answer is correct, render the correct answer as checked 'correct'
-        if (playerAnswer === answer && answer === props.correctAnswer) {
+        if (props.playerAnswer === answer && answer === props.correctAnswer) {
             return (
                 <div key={id} className="button-correct">
                     <input type="radio" id={id} name={`answer-${props.id}`} value={answer} disabled checked />
@@ -61,7 +49,7 @@ function Question(props) {
             );
         
         // If the player's answer is incorrect, render the incorrect answer as checked 'incorrect'
-        } else if (playerAnswer === answer && props.incorrectAnswers.includes(answer)) {
+        } else if (props.playerAnswer === answer && props.incorrectAnswers.includes(answer)) {
             return (
                 <div key={id} className="button-incorrect">
                     <input type="radio" id={id} name={`answer-${props.id}`} value={answer} disabled checked />
