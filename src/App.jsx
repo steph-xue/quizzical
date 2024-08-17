61f74ffbc031357856ac9999fd692ccd474a9ffa
Stephanie Xue <stephaniexue3@gmail.com>
2024-08-16 18:51:47 -0700

    add dependencies to decode question titles

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,28 @@ function App() {
 
   const [start, setStart] = React.useState(false);
   const [showAnswers, setShowAnswers] = React.useState(false);
+  const [questions, setQuestions] = React.useState([]);
+
+  React.useEffect(() => {
+    if (start) {
+      fetch('https://opentdb.com/api.php?amount=5&difficulty=medium')
+        .then(response => response.json())
+        .then(data => {
+          setQuestions(data.results);
+        });
+    }
+  }, [start]);
+
+  const questionElements = questions.map((question, index) => (
+    <Question
+      key={index}
+      question={question.question}
+      correctAnswer={question.correct_answer}
+      incorrectAnswers={question.incorrect_answers}
+      showAnswers={showAnswers}
+    />
+  ));
+
 
   return (
     <main>
@@ -19,9 +41,7 @@ function App() {
       {
         ((start && !showAnswers) || (!start && showAnswers)) &&
         <div className="questions-page">
-          <Question />
-          <Question />
-          <Question />
+          {questionElements}
           <button className="btn btn-black btn-check">Check answers</button>
         </div>
       }
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {decode} from 'html-entities';
 
-function Question() {
+function Question(props) {
+
+    const allAnswers = [...props.incorrectAnswers, props.correctAnswer]
+    console.log(props.incorrectAnswers)
+
+    const allAnswerElements = allAnswers.map(answer => (
+        <input type="radio" name="answer" value={answer} />
+    ));
+
     return (
-        <div className="question">
-            <h2>Question</h2>
-            <p>Answers</p>
+        <div className="question-container">
+            <h2 className="question">{decode(props.question)}</h2>
+            {allAnswerElements}
             <div className="line"></div>
         </div>
     );
